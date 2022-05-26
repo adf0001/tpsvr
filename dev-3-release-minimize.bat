ad278d4acfc9f28ff9d774da2e441b5fdbb6ce1d
fwg <adf0001@163.com>
2022-05-26 11:16:39 +0800

    change: rename release batch tool

--- dev-3-release-minimize.bat
+++
@@ -1,8 +0,0 @@
-
-call dev-1-client-bundle-minimize.bat
-
-call dev-1-client-bundle-minimize-latest-compatible.bat
-
-call dev-1-server-bundle-minimize.bat
-
-pause
---
+++ dev-3.1-release-minimize.bat
@@ -0,0 +1,8 @@
+
+call dev-1.3-client-bundle-minimize.bat
+
+call dev-1.4-client-bundle-minimize-latest-compatible.bat
+
+call dev-1.6-server-bundle-minimize.bat
+
+pause
